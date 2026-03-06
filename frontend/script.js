99ff988ba1813d80f3f89f4e627f91c2279219d2
Akshit Pratiush <121097688+aksprat@users.noreply.github.com>
2026-03-06 10:18:08 +0000

    new update

--- frontend/script.js
+++ frontend/script.js
@@ -5,13 +5,15 @@ form.addEventListener("submit", async (e) => {
 e.preventDefault()
 
 document.getElementById("loading").innerHTML =
-"⚡ Generating your Cloud Hero..."
+"⚡ Generating your Cloud Superhero..."
 
 const formData = new FormData(form)
 
 const res = await fetch("/generate-avatar", {
+
 method: "POST",
 body: formData
+
 })
 
 const data = await res.json()
@@ -19,6 +21,6 @@ const data = await res.json()
 document.getElementById("loading").innerHTML = ""
 
 document.getElementById("result").innerHTML =
-`<img src="${data.image}" width="400"/>`
+`<img src="${data.image}" />`
 
 })
